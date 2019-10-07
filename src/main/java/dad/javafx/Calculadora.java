708e2643211acde339155e8393130c2d0520e571
Victor Bethencourt <victolo500@gmail.com>
2019-10-07 08:34:02 +0100

    update

--- src/main/java/dad/javafx/Calculadora.java
+++ src/main/java/dad/javafx/Calculadora.java
@@ -1,6 +1,7 @@
 package dad.javafx;
 
 import javafx.application.Application;
+import javafx.beans.property.DoubleProperty;
 import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -45,7 +46,7 @@ public class Calculadora extends Application {
 		text5 = new TextField();
 		text5.setPromptText("0");
 		text5.setMaxWidth(50);
-
+		
 		text6 = new TextField();
 		text6.setPromptText("0");
 		text6.setMaxWidth(50);
@@ -57,7 +58,7 @@ public class Calculadora extends Application {
 
 		comboOperando = new ComboBox<String>();
 		comboOperando.getItems().addAll("+", "-", "*", "/");
-		comboOperando.setMaxWidth(50);
+		comboOperando.setMaxWidth(60);
 
 		VBox primerBox = new VBox(5, comboOperando);
 		primerBox.setAlignment(Pos.CENTER);
@@ -91,12 +92,16 @@ public class Calculadora extends Application {
 	}
 
 	private void onresultBnAcction(ActionEvent e) {
-//		Complejo num1, num2, num3, num4;
+		Complejo num1 = new Complejo();
+		Complejo num2 = new Complejo();
+		Complejo num3 = new Complejo();
+		Complejo num4 = new Complejo();
+
+		num1.setReal(Double.parseDouble(text1.getText()));
+		num2.setImaginario((DoubleProperty) text2.getProperties());
+		num3.setReal(Double.parseDouble(text3.getText()));
+		num4.setImaginario((DoubleProperty) text4.getProperties());
 		
-//		num1.setReal(Double.parseDouble(text1.getText()));
-//		num2.setImaginario((DoubleProperty) text2.getProperties());
-//		num3.setReal(Double.parseDouble(text3.getText()));
-//		num4.setImaginario((DoubleProperty)text4.getProperties());
 		
 		
 	}
